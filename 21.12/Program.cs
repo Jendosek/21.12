d5794d54e9fbe27307c478d6bdc790e446bd9aae
Jendosek <bala01081977@gmail.com>
2025-01-10 22:59:54 +0200

    prac5

--- 21.12/Program.cs
+++ 21.12/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            
+            int[] numbers = { -5, 10, 0, -8, -5, 7, -15, 3, -8 };
+
+            Func<int[], int[]> getUniqueNegativeNumbers = arr => arr.Where(num => num < 0).Distinct().ToArray();
+
+            int[] uniqueNegativeNumbers = getUniqueNegativeNumbers(numbers);
+
+            Console.WriteLine("Унікальні негативні числа:");
+            Console.WriteLine(string.Join(", ", uniqueNegativeNumbers));
         }
     }
 }
--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -7,6 +7,11 @@ public class UnitTest1
     [TestMethod]
     public void TestMethod1()
     {
-
+        int[] numbers = { -5, 10, 0, -8, -5, 7, -15, 3, -8 };
+        Func<int[], int[]> getUniqueNegativeNumbers = arr => arr.Where(num => num < 0).Distinct().ToArray();
+        
+        int[] uniqueNegativeNumbers = getUniqueNegativeNumbers(numbers);
+        
+        CollectionAssert.AreEqual(new int[] { -5, -8, -15 }, uniqueNegativeNumbers, "The unique negative numbers are incorrect.");
     }
 }
