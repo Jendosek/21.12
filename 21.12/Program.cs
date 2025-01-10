0bb3c176a7348626e4f4e210552f0959160a79c5
Jendosek <bala01081977@gmail.com>
2025-01-10 22:49:58 +0200

    dev

--- 21.12/Program.cs
+++ 21.12/Program.cs
@@ -4,33 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Func<string, (int R, int G, int B)> getRainbowColor = color =>
-            {
-                return color.ToLower() switch
-                {
-                    "червоний" => (255, 0, 0),
-                    "оранжевий" => (255, 165, 0),
-                    "жовтий" => (255, 255, 0),
-                    "зелений" => (0, 255, 0),
-                    "блакитний" => (0, 127, 255),
-                    "синій" => (0, 0, 255),
-                    "фіолетовий" => (139, 0, 255),
-                    _ => (-1, -1, -1) // Значення для невідомого кольору
-                };
-            };
-
-            Console.WriteLine("Введіть колір веселки:");
-            string colorInput = Console.ReadLine();
-
-            var (R, G, B) = getRainbowColor(colorInput);
-            if (R == -1 && G == -1 && B == -1)
-            {
-                Console.WriteLine("Невідомий колір.");
-            }
-            else
-            {
-                Console.WriteLine($"RGB для кольору \"{colorInput}\": ({R}, {G}, {B})");
-            }
+            
         }
     }
 }
