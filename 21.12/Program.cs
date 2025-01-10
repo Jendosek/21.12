208e1caaa63fb1ff15f3ff394c5ba7f519932799
Jendosek <bala01081977@gmail.com>
2025-01-10 22:53:12 +0200

    prac3

--- 21.12/Program.cs
+++ 21.12/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
+            int[] numbers = { 14, 21, 35, 40, 49, 50, 63, 70, 100 };
             
+            Func<int[], int> countMultiplesOfSeven = arr => arr.Count(num => num % 7 == 0);
+            
+            int count = countMultiplesOfSeven(numbers);
+
+            Console.WriteLine($"Кількість чисел, кратних семи: {count}");
         }
     }
 }
